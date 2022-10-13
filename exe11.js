833052f7750ac558495bce1b9fd91c1e6aa7dff5
ed <erineia@live.com>
2022-10-13 18:40:33 -0300

    exe12 e 13

--- exe11.js
+++ exe11.js
@@ -8,3 +8,27 @@
         mensagem e retornando true ou false.
 
 */
+
+function calcularAnoBissexto(ano){
+if(ano<= 0){
+    return false
+}else{
+   if((ano%4 === 0) && (ano%100 !== 0)){
+       return true
+   }else if(ano%400 ===0 ){
+      return true
+   }else{
+     return false
+   }
+}
+
+}
+
+calcularAnoBissexto(1900)
+console.log(calcularAnoBissexto(0))
+console.log(calcularAnoBissexto(4))
+console.log(calcularAnoBissexto(100))
+console.log(calcularAnoBissexto(400))
+console.log(calcularAnoBissexto(800))
+console.log(calcularAnoBissexto(2020))
+console.log(calcularAnoBissexto(2021))
